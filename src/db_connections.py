013360ad626b44dd825f717a6bd1d4e14ed38527
unknown <arimaproject7@gmail.com>
2023-11-25 13:11:30 -0300

    agregamos ejecucion de querys

--- src/db_connections.py
+++ src/db_connections.py
@@ -5,32 +5,47 @@ DB= "db"
 USER = "user"
 PASSWORD= "pss"
 id_numerico = 21
-conexion = psycopg2.connect(host=HOST, database=DB, user=USER, password=PASSWORD)
+
+
 
 anio_desde = 21
 anio_hasta= 21
 
 
-get_to_from_historic_query = f""" 
-    select confarimacabezalhistoricodesde as aniodesde, confarimacabezalhistoricohasta as aniohasta
-    from confarimacabezal
-    where confarimacabezalid= {id_numerico} 
-        """
+def get_to_from(conn, id_numerico):
 
-get_to_from_forecast_query = f""" 
-    select confarimacabezalproyecciondesde as aniodesde, confarimacabezalproyeccionricohasta as aniohasta
-    from confarimacabezal
-    where confarimacabezalid= {id_numerico} 
-        """
-
-get_data_query = f""""
-        select anionro as anio, datoshistoricoejincmes as mes, datoshistoricoejinccredvi as valor
-        from datoshistoricoejinc
-        where anionro >= {anio_desde} and anionro<= {anio_hasta} and datoshistoricoejincindice = {id_numerico}
-        """
-
-
-def get_to_from(query,conn):
+    get_to_from_historic_query = f""" 
+         select confarimacabezalhistoricodesde as aniodesde, confarimacabezalhistoricohasta as aniohasta
+         from confarimacabezal
+         where confarimacabezalid= {id_numerico} 
+             """
     cur = conn.cursor()
-    cur.execute(query)
+    cur.execute(get_to_from_historic_query)
+def get_forecast_year(conn,id_numerico):
+    get_to_from_forecast_query = f""" 
+        select confarimacabezalproyecciondesde as aniodesde, confarimacabezalproyeccionricohasta as aniohasta
+        from confarimacabezal
+        where confarimacabezalid= {id_numerico} 
+            """
+    cur = conn.cursor()
+    cur.execute(get_to_from_forecast_query)
+def get_data_forecast(conn,anio_desde, anio_hasta,id_numerico ):
+    get_data_query = f""""
+            select anionro as anio, datoshistoricoejincmes as mes, datoshistoricoejinccredvi as valor
+            from datoshistoricoejinc
+            where anionro >= {anio_desde} and anionro<= {anio_hasta} and datoshistoricoejincindice = {id_numerico}
+            """
+    cur = conn.cursor()
+    cur.execute(get_data_query)
 
+def get_conn(host, db, user, password):
+    print("conecting to db..")
+    conexion = psycopg2.connect(host=host, database=db, user=user, password=password)
+    return conexion
+
+def get_data(host, db, user, password, id_numerico):
+    conn = get_conn(host, db, user, password)
+    forecast_year = get_forecast_year(conn, id_numerico)
+    anio_desde= forecast_year[0]
+    anio_hasta = forecast_year[1]
+    return get_data_forecast(conn, anio_desde, anio_hasta, id_numerico)
